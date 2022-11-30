f1f16b92a40d39cfccadd4da5955491a7ca2bca2
TimHartsuijker <tim.hartsuijker@rocdeleijgraaf.nl>
2022-11-30 13:00:38 +0100

    database  toegevoegd

---
+++ de-blauwe-loper.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 30 nov 2022 om 12:57
+-- Serverversie: 10.4.25-MariaDB
+-- PHP-versie: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `de-blauwe-loper`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `game`
+--
+
+CREATE TABLE `game` (
+  `id` int(8) UNSIGNED NOT NULL,
+  `white_user_id` int(8) UNSIGNED DEFAULT NULL,
+  `black_user_id` int(8) UNSIGNED DEFAULT NULL,
+  `winner_user_id` int(8) UNSIGNED DEFAULT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `premission`
+--
+
+CREATE TABLE `premission` (
+  `id` int(8) UNSIGNED NOT NULL,
+  `user_id` int(8) UNSIGNED NOT NULL,
+  `premission` enum('members','games') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(8) UNSIGNED NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `password_hash` varchar(400) NOT NULL,
+  `member` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `white_user_id` (`white_user_id`),
+  ADD KEY `black_user_id` (`black_user_id`),
+  ADD KEY `winner_user_id` (`winner_user_id`);
+
+--
+-- Indexen voor tabel `premission`
+--
+ALTER TABLE `premission`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexen voor tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `game`
+--
+ALTER TABLE `game`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `premission`
+--
+ALTER TABLE `premission`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`white_user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`black_user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `game_ibfk_3` FOREIGN KEY (`winner_user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Beperkingen voor tabel `premission`
+--
+ALTER TABLE `premission`
+  ADD CONSTRAINT `premission_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
