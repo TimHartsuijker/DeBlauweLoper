d4e08dafc4219dca246a6156978df221442ffaf3
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-11-30 12:59:59 +0100

    header

--- src/required/header.php
+++ src/required/header.php
@@ -1,1 +1,11 @@
-<?php
+<nav class="navbar navbar-expend-md navbar-light shadow-sm kleur-header">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <img src="<?= ROOT ?>/img/pion.png" width="10px">
+                <h2> hallo</h2>            
+        </div>
+    </div>
+</nav>
