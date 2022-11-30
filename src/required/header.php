a562c85eb5f86054204c562cbb8b589d7e5ba9e2
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-11-30 14:19:45 +0100

    header verder

--- src/required/header.php
+++ src/required/header.php
@@ -1,11 +1,9 @@
-<nav class="navbar navbar-expend-md navbar-light shadow-sm kleur-header">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-
-            </div>
-            <img src="<?= ROOT ?>/img/pion.png" width="10px">
-                <h2> hallo</h2>            
-        </div>
-    </div>
+<nav class="navbar shadow-sm kleur-header">
+            <div class="ms-5">
+                <a href="<?= ROOT ?> " class="navbar-brand">
+                        <img src="<?= ROOT ?>/img/pion.png" width="80px" height="75" class="d-inline-block align-top logo">
+                </a>
+            </div>    
+            <div class="col-md-11"><h3><em>De Blauwe Loper</em></h3></div>                
 </nav>
+
