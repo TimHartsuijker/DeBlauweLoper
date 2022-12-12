f33781877bd39d066bcaddd8e119072b3b7232cf
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-12-12 15:20:44 +0100

    login opmaak klaar

--- src/pages/member/account/login.php
+++ src/pages/member/account/login.php
@@ -1,14 +1,35 @@
-<div class="text-center container">
-    <form method="post" class="form-signin">
-        <img class="mb-4" src="<?= ROOT ?>/img/loper.png" width="100vh" height="100vh">
-        <h1 class="h3 mb-3 font-weight-normal">Log in</h1>
-        <label for="inputName" class="sr-only"></label>   
-        <input type="text" id="inputName" class="form-control" placeholder="Naam" name="name" required autofocus="">
-        <label for="inputPassword" class="sr-only"></label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login" value="login">Inloggen</button>
-    </form>    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5 col-lg-4 achtergrond-box shadow rounded">
+            <div class="wrap">
+                    <img class="img mx-auto d-block mt-5" src="<?= ROOT ?>/img/loper.png" width="100vh" height="100vh">            
+                <div class="login-wrap p-4 p-md-5">
+                    <div class="d-flex">
+                        <div class="w-100">
+                            <h3 class="mb-8">Login</h3>
+                        </div>
+                    </div>
+                    <form method="post" class="signin-form">
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Naam">Naam:</label> 
+                            <input type="text" id="Naam" class="form-control" name="name" placeholder="Naam" required>            
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Wachtwoord">Wachtwoord:</label>
+                            <input type="password" id="Wachtwoord" class="form-control" name="password" placeholder="Wachtwoord" required>                        
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="form-control btn rounded submit px-3 mt-2">Inloggen</button>
+                        </div>
+                    </form>
+                    <p class="text-center mt-2">Nog geen account? <a href="registreer">Regristreren</a></p>                
+                </div>
+            </div>
+        </div>         
+    </div>
+   
 </div>
+
 
 <?php
 if (!empty($_POST)) 
@@ -23,6 +44,7 @@ if (!empty($_POST))
     }
     
 }
+
 
 
 ?>
