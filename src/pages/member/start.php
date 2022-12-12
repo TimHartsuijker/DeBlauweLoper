88d4f812f52de4ea05f78dca5e122118210067c2
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-12-12 19:43:44 +0100

    start pagina klaar

---
+++ src/pages/member/start.php
@@ -0,0 +1,34 @@
+    <div class="container mt-5 me-5">
+        <section class=" mt-5">
+            <h1> Welkom bij <em>De Blauwe Loper</em></h1>
+        </section>
+        <div class="row mt-5">   
+            <p class="col-lg-6">
+                Het moet ergens in 1980 geweest zijn dat een aantal schaakliefhebbers elkaar vonden en besloten een echte schaakvereniging op te richten. 
+                Er werd een voorlopig bestuur gekozen en het plaatselijke café wilde graag de nieuwe vereniging onderdak verlenen. 
+                Zij waren zelfs zo enthousiast dat zij een donatie deden om de vereniging ook financieel op poten te zetten. 
+                In het begin brachten de leden hun eigen schaakbord en schaakstukken mee naar de wekelijkse schaakavond. 
+                Al snel konden er, door de donaties en contributie, borden en spellen op naam van de club gekocht worden. 
+                In 1981 was het zover; de officiële oprichting van Schaakvereniging De Blauwe Loper.
+            </p>
+        </div>
+        <div class="row">
+            <p class="col-lg-6">
+                Waar de vereniging toen voor stond, en nu nog, is de liefde voor het edele schaakspel. 
+                Liever een mooie partij verliezen dan een saaie partij winnen, waardoor de strijd op het schaakbord niet geschuwd wordt. 
+                In iedere partij wordt keihard gestreden voor de winst maar naderhand wordt meestal de strijdbijl begraven onder het genot van een drankje aan de gezellige toog van de Keurvorst. 
+                Dat is de essentie van onze vereniging; een vriendenclub met respect voor elkaar, ongeacht afkomst, nationaliteit, rating of wat dan ook... Het is het spelletje wat ons bindt en verenigt.
+            </p>
+        </div>
+        <div class="row">
+            <p class="col-lg-6">
+                Wij spelen op donderdagavond onze interne competitie. 
+                We starten om 20.00 uur, met 1.45 uur voor 40 zetten per persoon. 
+                Mocht de beslissing dan nog niet gevallen zijn, dan krijgt men per persoon 15 minuten extra om de partij uit te spelen. 
+                Dus in theorie kan een partij (mits op tijd begonnen) nooit langer duren dan middernacht. 
+                <br><br>
+
+                Geïnteresseerd? Kom langs op onze clubavond.
+            </p>
+        </div>
+    </div>
