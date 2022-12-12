f1598c5da29c6b41520f105fccb1a09211413240
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-12-12 20:20:06 +0100

    registreer opmaak klaar

--- src/pages/member/account/registreer.php
+++ src/pages/member/account/registreer.php
@@ -1,37 +1,49 @@
-<form method="post">
-    <label>
-        gebruikersnaam:
-        <input type="text" name="name" required>
-    </label>
-
-    <label>
-        E-mail:
-        <input type="text" name="mail" required>
-    </label>
-
-    <label>
-        geboortedatum:
-        <input type="date" name="birth" required>
-    </label>
-
-    <label>
-        telefoonnummer:
-        <input type="text" name="phone" required>
-    </label>
-
-    <label>
-        wachtwoord:
-        <input type="password" name="password" required>
-    </label>
-
-    <label>
-        wachtwoord herhalen:
-        <input type="password" name="passwordRepeat" required>
-    </label>
-
-    <input type="submit" name="register" value="registreren" required>
-
-</form>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5 col-lg-4 achtergrond-reg shadow rounded">
+            <div class="wrap">
+                    <img class="img mx-auto d-block mt-5" src="<?= ROOT ?>/img/loper.png" width="100vh" height="100vh">            
+                <div class="login-wrap p-4 p-md-5">
+                    <div class="d-flex">
+                        <div class="w-100">
+                            <h3 class="mb-8">Registreren</h3>
+                        </div>
+                    </div>
+                    <form method="post" class="signin-form">
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Naam">Gebruikersnaam:</label> 
+                            <input type="text" id="Naam" class="form-control" name="name" placeholder="Naam" required>            
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="mail">E-mail:</label> 
+                            <input type="text" id="mail" class="form-control" name="mail" placeholder="E-mail" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="birth">Geboortedatum:</label> 
+                            <input type="date" id="birth" class="form-control" name="birth" placeholder="Geboortedatum" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="phone">Telefoonnummer:</label> 
+                            <input type="text" id="phone" class="form-control" name="phone" placeholder="Telefoonnummer" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Wachtwoord">Wachtwoord:</label>
+                            <input type="password" id="Wachtwoord" class="form-control" name="password" placeholder="Wachtwoord" required>                        
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="passwordRepeat">Wachtwoord herhaal:</label>
+                            <input type="password" id="passwordRepeat" class="form-control" name="passwordRepeat" placeholder="Wachtwoord herhaal" required>                            
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="registreren" class="form-control btn rounded submit px-3 mt-2">Inloggen</button>
+                        </div>
+                    </form>
+                    <p class="text-center mt-2">Al een account? <a href="login">Login</a></p>                
+                </div>
+            </div>
+        </div>         
+    </div>
+</div>
 
 <?php
 if(!empty($_POST)){
