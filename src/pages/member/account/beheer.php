9b8f2cc9526eb70715fa8edac3b9d77554f8fef2
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-12-13 12:55:14 +0100

    opmaak beheer

--- src/pages/member/account/beheer.php
+++ src/pages/member/account/beheer.php
@@ -11,19 +11,35 @@ if(!empty($_POST))
 }
 ?>
 <div class="container">
-    <form action="" method="post">
-        Account Beheren
-        <div class="form-group">
-            <input type="text" name="name" class="form-control" value="<?=$user->getName()?>">
-        </div>
-        <div class="form-group">
-            <input type="text" name="mail" class="form-control" value="<?=$user->getEmail()?>">
-        </div>
-        <div class="form-group">
-            <input type="password" name="password" class="form-control" value="<?=$user->getPasswordHash()?>">
-        </div>
-        <div class="form-group">
-            <input type="submit" name="update" class="form-control btn" value="Update">
-        </div>
-    </form>
+    <div class="row justify-content-center">
+        <div class="col-md-5 col-lg-4 achtergrond-box shadow rounded">
+            <div class="wrap">
+                    <img class="img mx-auto d-block mt-5" src="<?= ROOT ?>/img/loper.png" width="100vh" height="100vh">            
+                <div class="login-wrap p-4 p-md-5">
+                    <div class="d-flex">
+                        <div class="w-100">
+                            <h3 class="mb-8">Beheren</h3>
+                        </div>
+                    </div>
+                    <form method="post" class="signin-form">
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Naam">Naam:</label> 
+                            <input type="text" name="name" class="form-control" value="<?=$user->getName()?>">           
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Wachtwoord">Email:</label>
+                            <input type="text" name="mail" class="form-control" value="<?=$user->getEmail()?>">                       
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="form-control-placeholder" for="Wachtwoord">Wachtwoord:</label>
+                            <input type="password" name="password" class="form-control" value="<?=$user->getPasswordHash()?>">                      
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="update" value="update" class="form-control btn rounded submit px-3 mt-2">Bewerken</button>
+                        </div>
+                    </form>              
+                </div>
+            </div>
+        </div>         
+    </div>
 </div>
